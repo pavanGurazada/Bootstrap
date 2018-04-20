b6b7850063e454e39df3e555f618a7dd4b38e22f
pgurazada <pavankumar.gurazada@gmail.com>
2018-04-20 17:12:41 +0400

    initial code for tests...

---
+++ DH-Tests.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Chapter 4: Tests "
+#' author: Daivson and Hinkley
+#' output: github_document
+#' ---
+
+#' last update: Fri Apr 20 17:12:15 2018
+
+library(boot)
+library(tidyverse)
+
